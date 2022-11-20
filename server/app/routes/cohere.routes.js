712315f5a88ed2566e43ad7ebc285c17c0175ba8
Josh Rabovsky <jarsky.75@gmail.com>
2022-11-19 22:12:01 -0500

    Test endpoint

--- server/app/routes/cohere.routes.js
+++ server/app/routes/cohere.routes.js
@@ -5,6 +5,9 @@ module.exports = app => {
 
     // Get
     router.get("/:id", cohere.labelAccount)
+    router.get("/", (req, res) => {
+        return res.send("This went through")
+    })
 
     app.use('/api/cohere', router);
 };
